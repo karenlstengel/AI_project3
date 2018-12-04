c6ed8ff0987e117cd6751e173abf05406f5e8428
karenlstengel <#Justdance1>
2018-12-03 20:27:27 -0700

    added the sense of scream

--- AI_project3/src/Agent.java
+++ AI_project3/src/Agent.java
@@ -546,6 +546,7 @@ public class Agent {
         g.setWumpusLife(shoot(g));
 
         if( !g.isWumpusLife()) {
+            System.out.println("Scream!");
 
             //set the square 
             g.getGrid()[wumpus.getX()][wumpus.getY()].setWumpus(false);
@@ -586,8 +587,3 @@ public class Agent {
 
     }
 }
-
-//if gold take and bounce
-//move_to()
-//deal_with_wumpus()
-//choose_pit()
